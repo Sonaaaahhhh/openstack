acaacf6d1d106c551b0bef438f58f81634f48900
Sonaaaahhhh <sonasuresh811@gmail.com>
2026-02-03 01:44:21 +0530

    Add files via upload

--- part3/index.js
+++ part3/index.js
@@ -4,33 +4,33 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 
-// custom token for showing request body
+// custom morgan token to show body
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
 })
 
-// use morgan
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
-    id: 1,
+    id: "1",
     name: "Arto Hellas",
     number: "040-123456"
   },
   {
-    id: 2,
+    id: "2",
     name: "Ada Lovelace",
     number: "39-44-5323523"
   },
   {
-    id: 3,
+    id: "3",
     name: "Dan Abramov",
     number: "12-43-234345"
   },
   {
-    id: 4,
+    id: "4",
     name: "Mary Poppendieck",
     number: "39-23-6423122"
   }
@@ -41,7 +41,7 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-// GET info page
+// INFO
 app.get('/info', (req, res) => {
   const date = new Date()
   res.send(`
@@ -50,9 +50,9 @@ app.get('/info', (req, res) => {
   `)
 })
 
-// GET single person
+// GET one person
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
+  const id = req.params.id
   const person = persons.find(p => p.id === id)
 
   if (person) {
@@ -64,12 +64,17 @@ app.get('/api/persons/:id', (req, res) => {
 
 // DELETE person
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
+  const id = req.params.id
   persons = persons.filter(p => p.id !== id)
   res.status(204).end()
 })
 
-// POST new person
+// GENERATE ID
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000).toString()
+}
+
+// ADD person
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
@@ -80,23 +85,24 @@ app.post('/api/persons', (req, res) => {
   }
 
   const nameExists = persons.find(p => p.name === body.name)
+
   if (nameExists) {
     return res.status(400).json({
       error: 'name must be unique'
     })
   }
 
-  const newPerson = {
-    id: Math.floor(Math.random() * 100000),
+  const person = {
+    id: generateId(),
     name: body.name,
     number: body.number
   }
 
-  persons = persons.concat(newPerson)
-  res.json(newPerson)
+  persons = persons.concat(person)
+  res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
